2f5f70e38b76909223968ba88d36f5aaf09f6359
Markos <64923455+gravenuance@users.noreply.github.com>
2022-04-09 12:45:55 +0200

    Update ThreatWarn.lua

--- ThreatWarn.lua
+++ ThreatWarn.lua
@@ -2,6 +2,10 @@ local player_guid
 local unit_was_affecting_combat
 local unit_was_affecting_combat_when
 
+local single_target_threat_spells = {[33987] = 1, [30356] = 1, [30357] = 1, [26996]=1, [32700]=1, [20271]=1}
+local taunt_spells = {[31789]=1, [355]=1, [6795]=1}
+local aoe_threat_spells = {[5209]=1, [1161]=1}
+
 local timeSinceLastUpdate = 0
 local combatInterval = 0.1
 local function tw_combat_update(self, elapsed)
@@ -29,22 +33,30 @@ end
 local function tw_combat_log(...)
   local _, combat_event, _, src_guid = ...
   local spell_id, spell_name = select(12, ...)
-  if combat_event == "SPELL_MISSED" and src_guid == player_guid then
-    if (spell_id == 33987 or spell_id == 30356 or spell_id == 30357 or spell_id == 26996 or spell_id == 32700 or spell_id == 20271) then 
-      local get_time = GetTime()
-      if get_time - unit_was_affecting_combat_when <= 10 then
-        local miss_type = select(15, ...)
-        if (miss_type == "ABSORB") then
+  if (single_target_threat_spells[spell_id] ~= nil) then 
+    local get_time = GetTime()
+    if get_time - unit_was_affecting_combat_when <= 10 then
+      local msg
+      if combat_event == "SPELL_MISSED" and src_guid == player_guid then
+        msg = select(15, ...)
+        if (msg == "ABSORB") then
           return
         end
-        tw_send_msg(spell_name, miss_type)
+        tw_send_msg(spell_name, msg)
+      else
+        local crit = select(21, ...)
+        if crit then
+          tw_send_msg(spell_name, "CRITICAL")
+        end
       end
-    elseif (spell_id == 31789 or spell_id == 355 or spell_id == 6795) then
-      local miss_type = select(15, ...)
-      tw_send_msg(spell_name, miss_type)
+    end
+  elseif (taunt_spells[spell_id] ~= nil) then
+    if combat_event == "SPELL_MISSED" and src_guid == player_guid then
+      local msg = select(15, ...)
+      tw_send_msg(spell_name, msg)
     end
   end
-  if (src_guid == player_guid and (spell_id == 5209 or spell_id == 1161) and combat_event == "SPELL_CAST_SUCCESS") then
+  if (src_guid == player_guid and (aoe_threat_spells[spell_id]~=nil) and combat_event == "SPELL_CAST_SUCCESS") then
     tw_send_msg(spell_name, "CAST")
   end
 end
